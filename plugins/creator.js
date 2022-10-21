e1fbdaca00f697728fd5c5a32c2a55d237c0a232
BayuuOFC <bayurizkyananda65@gmail.com>
2022-10-21 13:06:12 +0700

    botwamenfess

---
+++ plugins/creator.js
@@ -0,0 +1,106 @@
+let { MessageType } = (await import('@adiwajshing/baileys')).default
+
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+  let type = (args[0] || '').toLowerCase()
+  let _type = (args[0] || '').toLowerCase()
+
+//------- NOMOR
+  let nowner = `${nomorown.split`@`[0]}@s.whatsapp.net`
+  let teksnomor = `${htki} *OWNER* ${htka}
+ @${nomorown.split`@`[0]} 
+------- ${nameown} -------
+
+ *Note:*
+• Owner tidak menerima save contact
+• Owner berhak blockir tanpa alasan
+• Berbicaralah yang sopan & tidak spam
+• Owner Hanya merespon yang berkaitan dengan BOT
+• No Telp`
+
+//------------ BIO
+let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
+let teksbio = `${htki} *BIODATA* ${htka}
+${htjava} * Nama* : Bayuuu
+${htjava} * Gender* : Boys
+${htjava} * Agama* : Islam
+${htjava} * Tanggal lahir* : 11 January
+${htjava} * Hobby* : Watch Anime.
+${htjava} * Sifat* : YTTA
+${htjava} * Tinggal* : Indonesia, Tangerang Selatan, Banten
+${htjava} * Waifu* : Nakano Yotsuba.
+
+${htjava} * s* : ${sig}
+${htjava} * :* ${sgh}
+•·––––––––––––––––––––––––––·•
+`
+  let teks = 'Pilih dibawah kak ! o()o'
+const sections = [
+   {
+	title: `${htjava} OWNER –––––––––·•`,
+	rows: [
+	{title: " • Nomor Owner", rowId: ".owner nomor"},
+	{title: " • Nomor Owner V2", rowId: ".whmods1"},
+	{title: " • Pengembang", rowId: ".whmods2"},
+	{title: " • Creator", rowId: ".whmods3"},
+	{title: " • Biodata", rowId: ".owner bio"},
+	{title: " • Script", rowId: ".sc"},
+	]
+    },{
+	title: `${htjava} SUPPORT ME –––––––·•`,
+	rows: [
+	    {title: " • Donasi", rowId: ".owner nomor"},
+	{title: " • Sewa", rowId: ".sewa"},
+	{title: " • Buy Premium", rowId: ".premium"},
+	]
+  },
+]
+
+const listMessage = {
+  text: teks,
+  footer: null,
+  title: `${htki} *OWNER* ${htka}`,
+  buttonText: "Click Here !",
+  sections
+}
+
+  try {
+    if (/(creator|owner)/i.test(command)) {
+      const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
+        switch (type) {
+        	case 'nomor':
+        conn.reply(m.chat, teksnomor, m, { contextInfo: { mentionedJid: [nowner] }})
+        break
+            case 'bio':
+          //conn.sendHydrated(m.chat, teksbio, wm, ppown, sig, "ðŸ“· Instagram", nomorown, 'ðŸŒ¹ Nomor', [[null, null], [null, null],[null,null]], m)
+
+          conn.sendButton(m.chat, teksbio, wm, ppown, [
+                ['Sewa Bot', `${usedPrefix}sewa`],
+                ['Menu', `${usedPrefix}menu`]
+            ], m)
+            break
+            
+          default:
+            return await conn.sendMessage(m.chat, listMessage, m, { contextInfo: { mentionedJid: [m.sender] }})
+        }
+    } else if (/enchant|enchan/i.test(command)) {
+      const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
+      switch (_type) {
+        case 't':
+          break
+        case '':
+          break
+
+        default:
+          return conn.sendButton( m.chat, caption, wm, null, [`â‹®â˜° Menu`, `.menu`], m)
+      }
+    }
+  } catch (err) {
+    m.reply("Error\n\n\n" + err.stack)
+  }
+}
+
+handler.help = ['bayuown', 'ownbay']
+handler.tags = ['main', 'info']
+handler.command = /^(bayuown|ownbay)/i
+
+export default handler
